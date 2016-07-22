9738759711a777e7eec9d74066d9affb459fe0aa
Soichi Tanabe <soichit@D-172-28-35-203.dhcp4.washington.edu>
2016-07-22 14:49:38 -0700

    encountered errors with forms

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from "../shared/ingredient";
+import {ShoppingListService} from "./shopping-list.service";
 
 @Component({
   moduleId: module.id,
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'shopping-list-add.component.html'
 })
 export class ShoppingListAddComponent implements OnInit {
+  isAdd = true;
+  item: Ingredient;
 
-  constructor() {}
+  constructor(private sls: ShoppingListService) {}
 
   ngOnInit() {
+  }
+
+  onSubmit(ingredient: Ingredient) {
+    console.log(ingredient);
+    if (!this.isAdd) {
+      //edit
+    } else {
+      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.sls.addItem(this.item);
+    }
   }
 
 }

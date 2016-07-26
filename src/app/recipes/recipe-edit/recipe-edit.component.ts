ce70cf8ae27c42337fc5f372f43e299fb12b50da
Soichi Tanabe <soichit@D-172-28-35-196.dhcp4.washington.edu>
2016-07-25 21:57:17 -0700

    half way thru forms tutorial

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {RecipeService} from "../recipe.service";
+import {Subscription} from "rxjs/Rx";
+import {Recipe} from "../recipe";
+import {FormArray, FormGroup, FormControl, Validators, FormBuilder} from "@angular/forms";
 
 @Component({
   moduleId: module.id,
@@ -6,11 +11,57 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: 'recipe-edit.component.html',
   styles: []
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
+  private recipeIndex: number;
+  private recipe: Recipe;
+  private isNew = true;
+  private subscription: Subscription;
 
-  constructor() {}
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService,
+              private formBuilder: FormBuilder) {}
 
   ngOnInit() {
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        if (params.hasOwnProperty('id')) {
+          this.isNew = false;
+          this.recipeIndex = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.recipeIndex);
+        } else {
+          this.isNew = true;
+          this.recipe = null;
+        }
+        //console.log(isNew);
+      }
+    );
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  private initForm(isNew: boolean) {
+    let recipeName = '';
+    let recipeImageUrl = '';
+    let recipeContent = '';
+    let recipeIngredients: FormArray = new FormArray([]);
+
+    if (!this.isNew) {
+      for (let i = 0; i < this.recipe.ingredients.length; i++) {
+        recipeIngredients.push(
+          new FormGroup({
+            name: new FormControl(this.recipe.ingredients[i].name, Validators.required),
+            amount: new FormControl(this.recipe.ingredients[i].amount, [
+              Validators.required,
+              Validators.pattern("\\d+")
+            ])
+          })
+        );
+      }
+      recipeName = this.recipe.name;
+      recipeImageUrl = this.recipe.imagePath;
+      recipeContent = this.recipe.description;
+    }
+  }
 }
--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 import {Ingredient} from "../shared/ingredient";
 import {ShoppingListService} from "./shopping-list.service";
 
@@ -7,23 +7,43 @@ import {ShoppingListService} from "./shopping-list.service";
   selector: 'rb-shopping-list-add',
   templateUrl: 'shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnChanges {
+  @Input() item: Ingredient;
+  @Output() cleared = new EventEmitter();
   isAdd = true;
-  item: Ingredient;
 
   constructor(private sls: ShoppingListService) {}
 
-  ngOnInit() {
+  ngOnChanges(changes) {
+    if (changes.item.currentValue === null) {
+      this.isAdd = true;
+      this.item = {name: null, amount: null};
+    } else {
+      this.isAdd = false;
+    }
   }
 
   onSubmit(ingredient: Ingredient) {
     console.log(ingredient);
+    const newIngredient = new Ingredient(ingredient.name, ingredient.amount);
+
     if (!this.isAdd) {
-      //edit
+      this.sls.editItem(this.item, newIngredient);
+      this.onClear();
     } else {
-      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.item = newIngredient;
       this.sls.addItem(this.item);
     }
+  }
+
+  onDelete() {
+    this.sls.deleteItem(this.item);
+    this.onClear();
+  }
+
+  onClear() {
+    this.isAdd = true;
+    this.cleared.emit(null);
   }
 
 }
